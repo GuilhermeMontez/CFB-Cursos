1d2a276ac23e2f73fb9b7c318d30295fc9aeba17
Guilherme Montes <guilmontez@outlook.com>
2021-06-10 14:06:58 -0300

    Incremento e decremento de Variáveis

---
+++ Aula17/script.js
@@ -0,0 +1,30 @@
+// Incremento e decremento de Variáveis
+// todas as operações matemáticas podem ser feitas
+
+var num = 10;
+
+// incremento 
+num = num + 1; // ou num++;
+
+// para incrementos maiores que 1
+num = num + 2; //ou num += 2
+
+// decremento 
+num = num -1; // ou num--;
+
+// para decremento maiores que 1
+num = num - 2 // ou num -= 2
+
+//pós incremento num++
+document.write(num++);  
+
+document.write("<br>")
+
+// pré incremento ++num
+document.write(++num) 
+
+document.write("<br>")
+
+// inverter valor
+num = num*-1; // ou num *=- 1 ou -num
+

b3f61b0936837b3194b84b6eec3b4f7e2c9cd7bc
Guilherme Montes <guilmontez@outlook.com>
2021-06-18 14:32:21 -0300

    Manipular elementos html com JavaScript

---
+++ Aula23/script.js
@@ -0,0 +1,14 @@
+// Manipular elementos html com JavaScript
+
+var txt = document.getElementById("texto");
+
+txt.style.color="#f00";
+txt.style.fontSize="50px";
+txt.style.display = "block";
+
+var cx = document.getElementById("caixa");
+
+cx.style.border = "1px solid #000";
+cx.style.width = "300px";
+cx.style.height = "300px";
+cx.style.backgroundColor = "#aaa";
